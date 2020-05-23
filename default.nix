22e6c7a6403cf68e1ba8974d17be1d82eaba0565
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-23 12:59:27 +0100

    add Nix package definition

---
+++ default.nix
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2020 Ethel Morgan
+#
+# SPDX-License-Identifier: MIT
+
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+buildGoModule rec {
+  name = "catbus-lgtv-${version}";
+  version = "latest";
+  goPackagePath = "github.com/ethulhu/catbus-lgtv";
+
+  modSha256 = "1s34h495h40vs90c79vdzk6pmpzskbmq0dqn4nw4iq7880i5s0z2";
+
+  src = ./.;
+
+  meta = {
+    homepage = "https://ethulhu.co.uk/catbus";
+    licence = stdenv.lib.licenses.mit;
+  };
+}

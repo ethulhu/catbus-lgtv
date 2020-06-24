6184414321f7f633dbb71cf70ed336e1c42d9d9e
Ethel Morgan <eth@ethulhu.co.uk>
2020-06-24 22:49:07 +0100

    fix nix-build

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ buildGoModule rec {
   version = "latest";
   goPackagePath = "go.eth.moe/catbus-lgtv";
 
-  modSha256 = "1s34h495h40vs90c79vdzk6pmpzskbmq0dqn4nw4iq7880i5s0z2";
+  modSha256 = "1ww9azrgbkcavybx97y1x0ajck8l60g6n7spv4zjnxj52cra85p4";
 
   src = ./.;
 
